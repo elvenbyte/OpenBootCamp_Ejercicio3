e30c4a84b62347daba16cdbfac97acbbf50b9426
David Moñino <damobe@gmail.com>
2022-07-03 16:26:22 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-	System.out.println("Hello world!");
+	Persona persona = new Persona();
+	persona.setEdad(18);
+	persona.setNombre("David");
+	persona.setTelefono("000000000");
+	System.out.println(persona.getEdad());
+	System.out.println(persona.getNombre());
+	System.out.println(persona.getTelefono());
     }
 }
+
